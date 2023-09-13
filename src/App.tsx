2e104d08ef59c497f17306f9738c76b370a45fc8
Roni Athayde <pontes014@gmail.com>
2023-09-13 00:32:23 -0300

    chore: initialize a projetc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
   
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl">upload.ai</h1>
+
+        <div>
+          <span>Desenvolvido com 💜 no NLW da Rocketseat</span>
+          <Button>Github</Button>
+        </div>
+      </div>
+    </div>
   )
 }
